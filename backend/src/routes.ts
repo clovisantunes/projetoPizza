7b5cf735a1575c8441e4e16411d82fdad9f3634e
Clovis antunes <clovissantanna@gmail.com>
2023-05-08 19:24:23 -0300

    Iniciando controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,7 @@
-import {Router, Request, Response} from 'express';
+import {Router} from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
 
-router.get('/teste',(req: Request,res: Response) =>{
-    throw new Error('Erro ao fazer essa requisição')
-})
-
+router.post('/users', new CreateUserController().handle)
 export {router};
