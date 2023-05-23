651f2293713ea2c90292f3fd67a3d7f86be472b9
Clovis antunes <clovissantanna@gmail.com>
2023-05-23 13:38:55 -0300

    continuando projeto

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,6 +1,6 @@
 import prismaClient from "../../prisma/Index";
 import { compare } from "bcryptjs";
-
+import {sign} from 'jsonwebtoken'
 
 interface AuthRequest{
     email: string;
@@ -29,7 +29,28 @@ class AuthUserService{
             throw new Error ("User/password incorrect")
         }
 
-        
+        // gerar token jwt para usuario 
+
+        const  token = sign(
+            {
+            name: user.name,
+            email:user.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                subject:user.id,
+                expiresIn:'30d'
+            }
+        )
+
+        return{
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            token: token
+        }
+
+
         return {ok :true }
     }
 }
