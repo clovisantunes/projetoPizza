e62a80d564ac518c21ffefe420161ec93b792586
Clovis antunes <clovissantanna@gmail.com>
2023-05-24 16:03:13 -0300

    configurando envio de imagens de produtos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,18 @@
 import {Router} from 'express';
+import multer from 'multer';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
-
+import { CreateProductController } from './controllers/product/CreateProductController';
+import uploadConfig from './config/Multer';
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./temp"))
+
 // Rotas do Usuario
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
@@ -17,5 +22,9 @@ router.get('/me',isAuthenticated, new DetailUserController().handle)
 // rotas de categorias
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+
+// Rotas de produtos
+router.post('/product', isAuthenticated,upload.single('file'), new CreateProductController().handle)
 
 export {router};
---
+++ backend/src/services/product/CreateProductService.ts
@@ -0,0 +1,16 @@
+import prismaClient from "../../prisma/Index";
+
+interface ProductRequest{
+    name: string;
+    price: string;
+    description: string;
+    banner: string;
+    category_id: string;
+}
+
+class CreateProductService{
+    async execute({name, price, description, banner, category_id}: ProductRequest){
+        return {ok:true}
+    }
+}
+export {CreateProductService}
