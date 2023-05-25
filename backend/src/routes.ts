636cf41db6e11f4abe3a77e4f430dccb66a073de
Clovis antunes <clovissantanna@gmail.com>
2023-05-25 14:52:11 -0300

    Continuando projeto

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,10 @@ import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+
+
 import uploadConfig from './config/Multer';
 
 const router = Router();
@@ -26,5 +30,10 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 // Rotas de produtos
 router.post('/product', isAuthenticated,upload.single('file'), new CreateProductController().handle)
+router.get('/category/products', isAuthenticated, new ListByCategoryController().handle)
+
+// rotas order
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+
 
 export {router};
--- backend/src/services/product/CreateProductService.ts
+++ backend/src/services/product/CreateProductService.ts
@@ -10,7 +10,18 @@ interface ProductRequest{
 
 class CreateProductService{
     async execute({name, price, description, banner, category_id}: ProductRequest){
-        return {ok:true}
-    }
+        
+        const product  = await prismaClient.product.create({
+            data:{
+                name: name,
+                price: price,
+                description: description,
+                banner: banner,
+                category_id: category_id
+            }
+        })
+
+        return product
+        }
 }
 export {CreateProductService}
